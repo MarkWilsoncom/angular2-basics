2e44a1f720bc29ea8edbcd144298547e857f2113
Mark Wilson <mw_saab@hotmail.com>
2018-01-02 12:25:46 +0100

    Commit1

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  dynAlign = 'right';
+  title = 'appxxx';
+  age = 50;
+  person = {
+    name: 'Mark',
+    age: 50
+  };
+
+  // if (age = 50) {
+  //   this.dynAlign = 'left';
+  // } else {
+  //   this.dynAlign = 'center';
+  // }
+}
